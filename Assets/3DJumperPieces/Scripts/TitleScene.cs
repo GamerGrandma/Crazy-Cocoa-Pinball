b8084abd4df6510cee5bf72d935a7e9cda08ce59
GamerGrandma <91760394+GamerGrandma@users.noreply.github.com>
2021-11-20 16:10:43 -0700

    new title scene
    created new title scene with start button.
    -removed title from old scene.
    -reorganized folders

---
+++ Assets/3DJumperPieces/Scripts/TitleScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+    
+    public void StartButton()
+    {
+       SceneManager.LoadScene(1);
+    }
+}
