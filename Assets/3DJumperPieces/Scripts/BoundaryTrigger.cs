e9f5a1c4cc3a3d489eb714c140b62f1cca01e8c5
GamerGrandma <91760394+GamerGrandma@users.noreply.github.com>
2022-01-05 16:53:10 -0700

    update
    bonus objects still active but can be reused.

--- Assets/3DJumperPieces/Scripts/BoundaryTrigger.cs
+++ Assets/3DJumperPieces/Scripts/BoundaryTrigger.cs
@@ -7,19 +7,21 @@ public class BoundaryTrigger : MonoBehaviour
     
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Floater"))
-        {
-            Debug.Log("object in boundary");
-            other.gameObject.SetActive(false);
-        }
+        other.gameObject.SetActive(false);
+        /* if (other.gameObject.CompareTag("Floater"))
+         {
+             Debug.Log("floater in boundary");
+             other.gameObject.SetActive(false);
+             if (other.gameObject.CompareTag("Bonus"))
+             {
+                 Debug.Log("bonus in boundary");
+                 other.gameObject.SetActive(false);
+             }
+         }*/
     }
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Bonus"))
-        {
-            Debug.Log("object in boundary");
-            other.gameObject.SetActive(false);
-        }
+        
     }
     
 }
